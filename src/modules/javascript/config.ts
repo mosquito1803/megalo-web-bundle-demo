eaed32d5ff416c688fee7f0bfe2d08ee5e430f3c
许世超 <hzxushch@corp.netease.com>
2019-03-23 16:12:20 +0800

    修改分类能力

---
+++ src/modules/javascript/config.ts
@@ -0,0 +1,169 @@
+const categoryList:object[] = [
+  {
+    id : 0,
+    name : 'General',
+    cname : '通用',
+    desc : '通用通用通用通用',
+    icon : ''
+  },
+  {
+    id : 1,
+    name : 'Data Entry',
+    cname : '数据录入',
+    desc : '数据录入数据录入数据录入',
+    icon : ''
+  },
+  {
+    id : 2,
+    name : 'Data Display',
+    cname : '数据展示',
+    desc : '数据展示数据展示数据展示数据展示',
+    icon : ''
+  },
+  {
+    id : 3,
+    name : 'feedback',
+    cname : '反馈',
+    desc : '反馈反馈反馈反馈反馈',
+    icon : ''
+  },
+  {
+    id : 4,
+    name : 'Navigation', 
+    cname : '导航',
+    desc : '导航导航导航导航导航',
+    icon : ''
+  }
+]
+const componentList:object[] = [
+  {
+    name : 'Button',
+    cname : '按钮',
+    category : 0
+  },
+  {
+    name : 'Icon',
+    cname : '图标',
+    category : 0
+  },
+  {
+    name : 'Layout',
+    cname : '布局',
+    category : 0
+  },
+  {
+    name : 'Radio',
+    cname : '单选框',
+    category : 1
+  },
+  {
+    name : 'Checkbox',
+    cname : '复选框',
+    category : 1
+  },
+  {
+    name : 'Input',
+    cname : '输入框',
+    category : 1
+  },
+  {
+    name : 'Picker',
+    cname : '选择',
+    category : 1
+  },
+  {
+    name : 'Date Picker',
+    cname : '时间选择',
+    category : 1
+  },
+  {
+    name : 'Rate',
+    cname : '评分',
+    category : 1
+  },
+  {
+    name : 'SearchBar',
+    cname : '搜索栏',
+    category : 1
+  },
+  {
+    name : 'Uploader',
+    cname : '图片上传',
+    category : 1
+  },
+  {
+    name : 'Switch',
+    cname : '开关',
+    category : 1
+  },
+  {
+    name : 'Modal',
+    cname : '对话框',
+    category : 2
+  },
+  {
+    name : 'Actionsheet',
+    cname : '动作面板',
+    category : 2
+  },
+  {
+    name : 'Loading',
+    cname : '加载',
+    category : 2
+  },
+  {
+    name : 'toast',
+    cname : '轻提示',
+    category : 2
+  },
+  {
+    name : 'List',
+    cname : '列表',
+    category : 3
+  },
+  {
+    name : 'Swipe',
+    cname : '轮播',
+    category : 3
+  },
+  {
+    name : 'Tag',
+    cname : '标记',
+    category : 3
+  },
+  {
+    name : 'Badge',
+    cname : '徽章',
+    category : 3
+  },
+  {
+    name : 'NoticeBar',
+    cname : '通告栏',
+    category : 3
+  },
+  {
+    name : 'ImagePreview',
+    cname : '图片预览',
+    category : 3
+  },
+  {
+    name : 'NavBar',
+    cname : '导航栏',
+    category : 4
+  },
+  {
+    name : 'Tabbar',
+    cname : '标签栏',
+    category : 4
+  },
+  {
+    name : 'Tabs',
+    cname : '标签页',
+    category : 4
+  },
+]
+
+export default {
+  categoryList,
+  componentList
+}
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,34 +1,31 @@
 <template>
   <div class="m-index">
     <ul>
-      <li v-for="item in componentList" :key="item">
+      <li v-for="item in categoryL" :key="item">
         <!-- <template platform="wechat">
           <a :href="'/pages/' + item + '/index'">{{ item }}</a>
         </template> -->
         <template platform="web">
-          <PhList :title="item" subTitle="描述文字" icon="right" leftIcon="time" @click.handleClick='handleClick(item)'></PhList>
+          <PhList :title="item.cname" :subTitle="item.desc" icon="right" leftIcon="time" @click.handleClick='handleClick(item.id)'></PhList>
         </template>
       </li>
     </ul>
   </div>
 </template>
 
-<style>
-.m-index li{height:1rem;line-height:1rem;font-size:0.3rem;}
-.m-index li a{position:relative;display:block;width:100%;color:#333;text-indent:0.3rem;overflow:hidden;}
-.m-index li a:after{content:'';position:absolute;bottom:0;left:0.3rem;width:100%;height:1px;background:#ccc;-webkit-translate:scaleY(0.5);}
-.m-index li:last-child a:after{display:none;}
-.m-component{position: relative;}
-</style>
-
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import components from '@/common/components/';
+import config from '@/modules/javascript/config';
 
 @Component
 
 export default class index extends Vue {
-  get componentList(){
+  get categoryL(){
+    return config.categoryList;
+  } 
+
+  get componentL(){
     return Object.keys(components);
   }
   handleClick(v:string){
